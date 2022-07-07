76c8ab08bb0492646d48af8fa8658ceac771b70b
ruben ketsman <ruben.ketsman@hotmail.be>
2022-07-07 18:33:07 +0200

    cors fix try

--- DAL/Repositories/FavoritesRepository.cs
+++ DAL/Repositories/FavoritesRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ADOToolbox;
+using AdoToolbox;
 using DAL.Interfaces;
 using DAL.Models;
 using DAL.Tools;
